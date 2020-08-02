8dbf59c9eca37e60d4e224011e5313618f960ee9
Arsharaj Chauhan <47565923+arsharaj@users.noreply.github.com>
2020-08-02 22:58:08 +0530

    publish my first website

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector("img");
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute("src");
+    if(mySrc == "images/firefox-logo.png") {
+        myImage.setAttribute("src", "images/firefox-logo.svg.png");
+    } else {
+        myImage.setAttribute("src", "images/firefox-logo.png");
+    }
+}
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    let myName = prompt("Please enter your name. ");
+    if(!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = "Mozilla is Cool! " + myName;    
+    }
+}
+
+if(!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla is Cool! " + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
